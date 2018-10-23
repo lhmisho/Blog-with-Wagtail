1188802ecd96620d24ca9bdb8a66176d7aad3e37
lhmisho <lokman@divine-it.net>
2018-10-23 18:55:04 +0600

    venv deleted

--- wagTailProj/.venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/lhmisho/myPythonProjects/blog-with-wagtail/wagTailProj/.venv/bin/python3.6m
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
